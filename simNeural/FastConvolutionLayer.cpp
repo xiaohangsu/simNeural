761cf6ffb2434aab1dc83bc202383214f680009c
xiaohangsu <sxhdragon@gmail.com>
2017-02-22 00:10:46 -0500

    Save temporary changes and switch v2.0 branch

--- simNeural/FastConvolutionLayer.cpp
+++ simNeural/FastConvolutionLayer.cpp
@@ -11,13 +11,44 @@
 #include "SigmoidLayer.hpp"
 FastConvolutionLayer::FastConvolutionLayer(){};
 
-FastConvolutionLayer::FastConvolutionLayer(const int t_input_row, const int t_input_col, const int t_kernel_number, const int t_kernel_row, const int t_kernel_col, const double t_lr, const int batch) {
+FastConvolutionLayer::FastConvolutionLayer(const int t_input_row, const int t_input_col, const int t_inputNum, const int t_kernel_number, const int t_kernel_row, const int t_kernel_col, const double t_lr, const int t_stride) {
+    m_input_row = t_input_row;
+    m_input_col = t_input_col;
+    m_input_num = t_inputNum;
+    m_learningRate = t_lr;
+    m_kernel_num = t_kernel_number;
+    m_kernel_row = t_kernel_row;
+    m_kernel_col = t_kernel_col;
+    m_kernel = Eigen::MatrixXd::Random(m_kernel_col * m_kernel_row * m_kernel_num, m_input_num);
+    m_output = Eigen::MatrixXd(m_kernel_num, (m_input_row - m_kernel_row + 1) * (m_input_col - m_kernel_col + 1));
+    
+    for (int i = 0; i < m_kernel_num; i++) {
+        Eigen::MatrixXd temp((m_input_row - m_kernel_row + 1), (m_input_col - m_kernel_col + 1));
+        m_outputVec.push_back(temp);
+        m_errorVec.push_back(temp);
+    }
+    m_input = Eigen::MatrixXd((m_input_row - m_kernel_row + 1) * (m_input_col - m_kernel_col + 1), m_kernel_row * m_kernel_col * m_input_num);
+}
+
+void FastConvolutionLayer::forward(std::vector<Eigen::MatrixXd> &t_input) {
+    
+    for(int i = 0; i < m_input.rows(); i++) {
+        for (int j = 0; j < m_input.cols() / (m_kernel_row * m_kernel_col); j++) {
+            m_input.block(i, j * (m_kernel_row * m_kernel_col), 1, (m_kernel_row * m_kernel_col));
+        }
+    }
+    m_output = m_kernel * m_input;
+    m_activateLayer->activate(m_output);
+    for (int i = 0; i < m_kernel_num; i++) {
+        m_outputVec[i] = Eigen::MatrixXd::Map(m_output.row(i).data(), (m_input_row - m_kernel_row + 1), (m_input_col - m_kernel_col + 1));
+    }
+}
+
+void FastConvolutionLayer::backward(std::vector<Eigen::MatrixXd> &t_preError, Eigen::MatrixXd &t_lastTheta) {
     
 }
 
-void FastConvolutionLayer::forward(Eigen::MatrixXd &t_input) {
-    
-}
+
 
 void FastConvolutionLayer::setActivateLayer(ACTIVATE_TYPE t_TYPE) {
     switch (t_TYPE) {
--- simNeural/FastConvolutionLayer.hpp
+++ simNeural/FastConvolutionLayer.hpp
@@ -17,24 +17,31 @@
 class FastConvolutionLayer {
 public:
     FastConvolutionLayer();
-    FastConvolutionLayer(const int t_inputRow, const int t_outputRow, const int t_kernel_number, const int t_kernel_row, const int t_kernel_col, double t_lr, int t_batch);
-    virtual void forward(Eigen::MatrixXd& t_input);
-    virtual void backward(Eigen::MatrixXd& t_preError, std::vector<Eigen::MatrixXd>& t_lastTheta);
+    FastConvolutionLayer(const int t_inputRow, const int t_inputCol, const int t_inputNum, const int t_kernel_number, const int t_kernel_row, const int t_kernel_col, double t_lr, int t_batch);
+    virtual void forward(std::vector<Eigen::MatrixXd>& t_input);
+    virtual void backward(std::vector<Eigen::MatrixXd>& t_preError, Eigen::MatrixXd& t_lastTheta);
     virtual void descentGradient(Eigen::MatrixXd&);
     void setLearningRate(const double t_lr);
     void setActivateLayer(ACTIVATE_TYPE t_TYPE);
     ActivateLayer getActivateLayer();
     
 private:
-    int m_row;
-    int m_col;
+    int m_input_row;
+    int m_input_col;
+    int m_input_num;
     int m_kernel_row;
     int m_kernel_col;
+    int m_kernel_num;
+    int m_stride;
     Eigen::MatrixXd m_kernel;
     double m_learningRate;
     ActivateLayer *m_activateLayer;
+    Eigen::MatrixXd m_input;
     Eigen::MatrixXd m_output;
     Eigen::MatrixXd m_error;
+    std::vector<Eigen::MatrixXd> m_outputVec;
+    std::vector<Eigen::MatrixXd>
+    m_errorVec;
 };
 
 #endif /* FastConvolutionLayer_hpp */
