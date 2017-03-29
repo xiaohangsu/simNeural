2bcb4f9088c6e4088ed456e786e18492a158bb2d
Xiaohang Su <sxhdragon@gmail.com>
2017-03-28 22:29:35 -0400

    clear redundant codes

--- simNeural/DebugTool.h
+++ simNeural/DebugTool.h
@@ -12,41 +12,9 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include <vector>
-#include "Layer.hpp"
-
 using namespace std;
-using namespace Eigen;
 namespace debug {
-//    void printError(Layer layer) {
-//        for (int i = 0; i < error.size(); i++) {
-//            cout << "Error :" << i + 1 << endl;
-//            cout << error[i] << endl << endl;
-//        }
-//    }
-//    
-//    void printOutput(Layer layer) {
-//        for (int i = 0; i < output.size(); i++) {
-//            cout << "Output :" << i + 1 << endl;
-//            cout << output[i] << endl << endl;
-//        }
-//    }
-//    
-//    void printKernel(ConvolutionalLayer layer) {
-//        vector<vector<MatrixXd>> kernels = layer.getKernels();
-//        for (int i = 0; i < kernels.size(); i++) {
-//            for (int j = 0; j < kernels[0].size(); j++) {
-//                cout << "(" << i << ", " << j << ")" << endl;
-//                cout << kernels[i][j] << endl << endl;
-//            }
-//        }
-//    }
-//    
-//    
-//    void printBasicInfo(Layer layer) {
-//        cout << "Layer info :\t" << "InputNum:" << layer.getInputNum() << "\t|\tOutputNum:" << layer.getOutputNum() << endl;
-//        cout << "\tBatch:" << layer.getBatch() << "\t|\tCount:" << layer.getCount() << endl;
-//        cout << endl;
-//    }
+
 }
 
 #endif /* DebugTool_h */
