ea0a708df6942030bc93c20c076b63c87fcc19b3
Xiaohang Su <sxhdragon@gmail.com>
2017-03-28 22:30:01 -0400

    Redefine type in namespace

--- simNeural/preDefine.h
+++ simNeural/preDefine.h
@@ -11,25 +11,35 @@
 #include <Eigen/Dense>
 #include <vector>
 
-typedef Eigen::MatrixXd                                  Matrix;
-typedef Eigen::MatrixXd&                                 Matrix_r;
-typedef const Eigen::MatrixXd                            Matrix_c;
-typedef const Eigen::MatrixXd&                           Matrix_cr;
-typedef std::vector<Eigen::MatrixXd>                     vec_Matrix;
-typedef std::vector<Eigen::MatrixXd>&                    vec_Matrix_r;
-typedef const std::vector<Eigen::MatrixXd>               vec_Matrix_c;
-typedef const std::vector<Eigen::MatrixXd>&              vec_Matrix_cr;
-typedef std::vector<std::vector<Eigen::MatrixXd>>        vec2_Matrix;
-typedef std::vector<std::vector<Eigen::MatrixXd>>&       vec2_Matrix_r;
-typedef const std::vector<std::vector<Eigen::MatrixXd>>  vec2_Matrix_c;
-typedef const std::vector<std::vector<Eigen::MatrixXd>>& vec2_Matrix_cr;
-
+namespace simNeural {
+    typedef Eigen::MatrixXd                                   Matrix;
+    typedef Eigen::MatrixXd&                                  Matrix_r;
+    typedef Eigen::MatrixXd&&                                 Matrix_rr;
+    typedef const Eigen::MatrixXd                             Matrix_c;
+    typedef const Eigen::MatrixXd&                            Matrix_cr;
+    typedef const Eigen::MatrixXd&&                           Matrix_crr;
+    typedef std::vector<Eigen::MatrixXd>                      vec_Matrix;
+    typedef std::vector<Eigen::MatrixXd>&                     vec_Matrix_r;
+    typedef std::vector<Eigen::MatrixXd>&&                    vec_Matrix_rr;
+    typedef const std::vector<Eigen::MatrixXd>                vec_Matrix_c;
+    typedef const std::vector<Eigen::MatrixXd>&               vec_Matrix_cr;
+    typedef const std::vector<Eigen::MatrixXd>&&              vec_Matrix_crr;
+    typedef std::vector<std::vector<Eigen::MatrixXd>>         vec2_Matrix;
+    typedef std::vector<std::vector<Eigen::MatrixXd>>&        vec2_Matrix_r;
+    typedef std::vector<std::vector<Eigen::MatrixXd>>&&       vec2_Matrix_rr;
+    typedef const std::vector<std::vector<Eigen::MatrixXd>>   vec2_Matrix_c;
+    typedef const std::vector<std::vector<Eigen::MatrixXd>>&  vec2_Matrix_cr;
+    typedef const std::vector<std::vector<Eigen::MatrixXd>>&& vec2_Matrix_crr;
+    typedef Eigen::Map<Eigen::MatrixXd>                       Map_Matrix;
+}
 
 const static int FCL_BIAS_NUM = 1;
 const static int FCL_THETA_RANDOM_DIV = 10;
 const static double FCL_BIAS_VALUE = 1.0;
 const static double CONV_BIAS_UPPERBOUND = 1.0;
 const static double CONV_BIAS_LOWERBOUND = -1.0;
-const static enum ACTIVATE_TYPE {SIGMOID, RELU};
+enum ACTIVATE_TYPE {SIGMOID, RELU};
+
+enum LAYER_TYPE { ACTIVATE, FULL_CONNECTION, OUT_LAYER, LAYER };
 
 #endif /* preDefine_h */
