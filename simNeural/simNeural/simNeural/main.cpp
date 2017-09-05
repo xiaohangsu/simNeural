668652ef6f248ea2ca9d000f0051ee90a47fb1db
xiaohangsu <sxhdragon@gmail.com>
2017-09-04 23:48:27 -0400

    data include

---
+++ simNeural/simNeural/simNeural/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  simNeural
+//
+//  Created by Xiaohang Su on 8/28/17.
+//  Copyright © 2017 XiaohangSu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
